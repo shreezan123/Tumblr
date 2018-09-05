82d637240cbd7e7f07ba91e04ecac4472cbd9af6
Shrijan Aryal <shrijan.aryal123@gmail.com>
2018-09-05 00:38:07 -0400

    Final commit

---
+++ flix_demo_03/PhotoCell.swift
@@ -0,0 +1,31 @@
+//
+//  PhotoCell.swift
+//  flix_demo_03
+//
+//  Created by Shrijan Aryal on 9/4/18.
+//  Copyright © 2018 Shrijan Aryal. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath as IndexPath) as! PhotoCell
+        
+        // Configure YourCustomCell using the outlets that you've defined.
+        return cell
+    }
+}
